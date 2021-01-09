90810b7e3dabd2dd4244d453a1f04a85eacfe157
Meet Patel <meetaiims@yahoo.com>
2021-01-09 13:40:30 -0600

    create knex instance in controller

--- src/app.js
+++ src/app.js
@@ -4,31 +4,31 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const logger = require('./logger');
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common';
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello, boilerplate!');
+  res.send('Hello, fin-stack!');
 });
 
-app.use(errorHandler = (error, req, res, next) => {
+app.use(
+  (errorHandler = (error, req, res, next) => {
     let response;
     if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error'}};
+      response = { error: { message: 'server error' } };
     } else {
-        console.error(error);
-        response = { message: error.message, error }
+      console.error(error);
+      response = { message: error.message, error };
     }
     res.status(500).json(response);
-
-})
+  })
+);
 
 module.exports = app;
