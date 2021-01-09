78c0d5d7c91667c786bbe6c2e49c47714471faba
Meet Patel <meetaiims@yahoo.com>
2021-01-09 15:34:15 -0600

    create usersService and usersRouter objects

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const logger = require('./logger');
+const usersRouter = require('./users/users-router');
 
 const app = express();
 
@@ -13,6 +14,8 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use(usersRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, fin-stack!');
