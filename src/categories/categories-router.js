d64acfe737748d85d030151f5e00a1640ec073f9
Meet Patel <meetaiims@yahoo.com>
2021-01-12 17:02:48 -0600

    add POST /api/categories/:userId endpoint to categoriesRouter

--- src/categories/categories-router.js
+++ src/categories/categories-router.js
@@ -13,6 +13,31 @@ categoriesRouter.route('/api/categories/:userId').get((req, res, next) => {
     .then((categories) => res.json(categories))
     .catch(next);
 })
-.post(())
+.post(bodyParser, (req, res, next) => {
+    const knexInstance = req.app.get('db');
+    const { category, type } = req.body;
+    const { userId } = req.params;
+    const newCategory = {
+        category,
+        type,
+        user_id: userId
+    }
+    if (!newCategory.user_id) {
+        logger.error('User id is required');
+        return res.status(400).send('Invalid data')
+    }
+    if (!newCategory.category) {
+        logger.error('Category is required');
+        return res.status(400).send('Invalid data')
+    }
+    if (!newCategory.type) {
+        logger.error('Type is required');
+        return res.status(400).send('Invalid data');
+    }
+
+    return CategoriesService.insertCategory(knexInstance, newCategory)
+            .then(category => res.status(201).json(category))
+            .catch(next);
+})
 
 module.exports = categoriesRouter;
