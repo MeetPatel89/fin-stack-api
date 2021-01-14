a1263cab23f47674be0127577cdb47d51c98ce70
Meet Patel <meetaiims@yahoo.com>
2021-01-14 02:31:26 -0600

    add insertTransactions method to transactionsService

--- src/transactions-service.js
+++ src/transactions-service.js
@@ -1,6 +1,9 @@
 const TransactionsService = {
     getTransactionsByUserId(knex, userId) {
         return knex.select('*').from('transactions').where({user_id: userId})
+    },
+    insertTransactions(knex, newTransaction) {
+        return knex.insert(newTransaction).into('transactions').returning('*');
     }
 }
 
