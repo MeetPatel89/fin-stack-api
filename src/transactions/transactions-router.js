9f73b7cd2549dc84419034525d049b22b603047f
Meet Patel <meetaiims@yahoo.com>
2021-01-14 02:47:29 -0600

    add POST /api/transactions/:userId endpoint to transactionsRouter

--- src/transactions/transactions-router.js
+++ src/transactions/transactions-router.js
@@ -6,14 +6,39 @@ const transactionsRouter = express.Router();
 const bodyParser = express.json();
 
 transactionsRouter
-    .route('/api/transactions/:userId')
-    .get((req, res, next) => {
-        const knexInstance = req.app.get('db');
-        const { userId } = req.params;
+  .route('/api/transactions/:userId')
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db');
+    const { userId } = req.params;
 
-        TransactionsService.getTransactionsByUserId(knexInstance, userId)
-            .then(transactions => res.json(transactions))
-            .catch(next);
-    })
+    TransactionsService.getTransactionsByUserId(knexInstance, userId)
+      .then((transactions) => res.json(transactions))
+      .catch(next);
+  })
+  .post(bodyParser, (req, res, next) => {
+    const knexInstance = req.app.get('db');
+    const { userId } = req.params;
+    const newTransaction = req.body;
+    Object.assign(newTransaction, { user_id: userId });
+    if (!newTransaction.category) {
+      logger.error('Category is required');
+      return res.status(400).send('Invalid data');
+    }
+    if (!newTransaction.accounts) {
+      logger.error('Accounts is required');
+      return res.status(400).send('Invalid data');
+    }
+    if (!newTransaction.amount) {
+      logger.error('Amount is required');
+      return res.status(400).send('Invalid data');
+    }
+    if (!newTransaction.date_time) {
+      logger.error('Datetime is required');
+      return res.status(400).send('Invalid data');
+    }
+    return TransactionsService.insertTransactions(knexInstance, newTransaction)
+      .then((transaction) => res.status(201).json(transaction))
+      .catch(next);
+  });
 
 module.exports = transactionsRouter;
