699a3faaae931e630f109c72dc9c3b543020f5bc
Meet Patel <meetaiims@yahoo.com>
2021-01-14 20:04:53 -0600

    change transactions-relation design to include column 'type'

--- seeds/seed.transactions.sql
+++ seeds/seed.transactions.sql
@@ -1,8 +1,8 @@
-INSERT INTO transactions (id, user_id, category, accounts, amount, date_time)
+INSERT INTO transactions (id, user_id, category, accounts, type, amount, date_time)
 VALUES 
-    (1, 1, 'Food', 'Cash', 200.50, '2021-01-11T22:22:52+00:00'),
-    (2, 1, 'Salary', 'Citizens Bank', 5000, '2021-01-05T22:22:52+00:00'),
-    (3, 2, 'Entertainment', 'Cash', 150.75, '2021-01-10T22:22:52+00:00'),
-    (4, 2, 'Bonus', 'Citizens Bank', 7000, '2021-01-09T22:22:52+00:00'),
-    (5, 1, 'Food', 'Cash', 20000, '2021-01-13T01:00:00+00:00'),
-    (6, 1, 'Salary', 'Citizens Bank', 1000.85, '2021-01-13T03:00:00+00:00')
+    (1, 1, 'Food', 'Cash', 'expense', 200.50, '2021-01-11T22:22:52+00:00'),
+    (2, 1, 'Salary', 'Citizens Bank', 'balance', 5000, '2021-01-05T22:22:52+00:00'),
+    (3, 2, 'Entertainment', 'Cash', 'expense', 150.75, '2021-01-10T22:22:52+00:00'),
+    (4, 2, 'Bonus', 'Citizens Bank', 'balance', 7000, '2021-01-09T22:22:52+00:00'),
+    (5, 1, 'Food', 'Cash', 'expense', 20000, '2021-01-13T01:00:00+00:00'),
+    (6, 1, 'Salary', 'Citizens Bank', 'balance', 1000.85, '2021-01-13T03:00:00+00:00')
